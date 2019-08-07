1a273a670b00d082b73bb86e1fce526abe879417
elliecherrill <ellie.cherrill@outlook.com>
2019-08-08 00:02:31 +0100

    Restructures pages

--- navBar.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("sideNavBar").style.width = "300px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("sideNavBar").style.width = "0";
-}
